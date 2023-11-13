53f7de34a3d183c693705cc1bfed7480320d2c2b
Maria Elena <111201881+MariaElenaMas@users.noreply.github.com>
2023-11-13 14:37:15 +0100

    Event component with summary, start time, and location display added

--- src/components/Event.js
+++ src/components/Event.js
@@ -1,7 +1,23 @@
-const Event = () => {
+import { useState } from 'react';
+
+const Event = ({ event }) => {
+    const [showDetails, setShowDetails] = useState(false);
     return (
-        <li></li>
+        <li className="event">
+            <h3>{event.summary}</h3>
+            <p>{event.start.dateTime}</p>
+            <p>{event.location}</p>
+
+            <button className='details-btn'
+                onClick={() => {
+                    setShowDetails(!showDetails);
+                }}
+            >
+                {showDetails ? 'Hide Details' : 'Show Details'}
+            </button>
+        </li>
     );
 }
 
 export default Event;
+
