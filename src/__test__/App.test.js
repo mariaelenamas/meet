986f447323d8c17f65725c3b61674b0783692776
Maria Elena <111201881+MariaElenaMas@users.noreply.github.com>
2023-11-16 21:51:57 +0100

    Update test to add new functionalities

--- src/__test__/App.test.js
+++ src/__test__/App.test.js
@@ -1,5 +1,6 @@
-import { render } from '@testing-library/react';
+import { render, waitFor, within } from '@testing-library/react';
 import App from '../App';
+import userEvent from '@testing-library/user-event';
 
 describe('<App /> component', () => {
     let AppDOM;
@@ -13,5 +14,25 @@ describe('<App /> component', () => {
 
     test('render CitySearch', () => {
         expect(AppDOM.querySelector('#city-search')).toBeInTheDocument();
+    });
+
+    test('number of events displayed updates based on user input', async () => {
+        const user = userEvent.setup();
+        const AppComponent = render(<App />);
+        const AppDOM = AppComponent.container.firstChild;
+
+        await waitFor(() => {
+            const NumberOfEventsDOM = AppDOM.querySelector('#events-count');
+            expect(NumberOfEventsDOM).toBeInTheDocument();
+        });
+
+        const NumberOfEventsDOM = AppDOM.querySelector('#events-count');
+        const NumberOfEventsInput = within(NumberOfEventsDOM).queryByRole('textbox');
+
+        await user.type(NumberOfEventsInput, '{backspace}{backspace}5');
+
+        const EventListDOM = AppDOM.querySelector('#event-list');
+        const allRenderedEventItems = within(EventListDOM).queryAllByRole('listitem');
+        expect(allRenderedEventItems.length).toEqual(39);
     });
 });
