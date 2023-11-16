f1d543ee71f577a564721617be98198c81b43200
Maria Elena <111201881+MariaElenaMas@users.noreply.github.com>
2023-11-16 21:53:17 +0100

    Update the App functionality

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
 import { useEffect, useState } from 'react';
+import { extractLocations, getEvents } from './api';
 import CitySearch from './components/CitySearch';
 import EventList from './components/EventList';
+import NumberOfEvents from './components/NumberOfEvents';
 import './App.css';
-import { extractLocations, getEvents } from './api';
+
 
 const App = () => {
   const [locations, SetLocations] = useState([]);
+  const [events, setEvents] = useState([]);
+
   const getAllEvents = async () => {
     const events = await getEvents()
     const AllLocations = extractLocations(events)
     SetLocations(AllLocations);
+    setEvents(events);
   }
 
   useEffect(() => {
@@ -20,7 +25,8 @@ const App = () => {
   return (
     <div className="App">
       <CitySearch allLocations={locations} />
-      <EventList />
+      <EventList events={events} />
+      <NumberOfEvents eventsNumber={events.legnth} />
     </div>
   );
 }
