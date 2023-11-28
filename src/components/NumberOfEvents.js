ad43ef431a35c15451b620aae0a74d2c786532dd
Maria Elena <111201881+MariaElenaMas@users.noreply.github.com>
2023-11-28 18:27:34 +0100

    handleInputChanged added

--- src/components/NumberOfEvents.js
+++ src/components/NumberOfEvents.js
@@ -1,14 +1,28 @@
-import { useState } from "react";
 import "./NumberOfEvents.css";
 
-const NumberOfEvents = ({ eventsNumber }) => {
-    const [number, setNumber] = useState(eventsNumber ? eventsNumber : 32)
+const NumberOfEvents = ({ setCurrentNOE, setErrorAlert }) => {
+    // const [number, setNumber] = useState(eventsNumber ? eventsNumber : 32)
+    const handleInputChanged = (event) => {
+        const value = event.target.value;
+
+        let infoText;
+        if (isNaN(value) || value > 50 || value <= 0) {
+            infoText = "Please enter a number between 0 and 50";
+            setErrorAlert(infoText);
+        } else {
+            infoText = "";
+            setCurrentNOE(value);
+            setErrorAlert(infoText);
+        }
+    };
+
     return (
         <div id="events-count">
             <p>Select Number of Events</p>
             <input
                 type="text"
                 defaultValue="32"
+                onChange={handleInputChanged}
                 data-testid="events-count"
             />
         </div>
