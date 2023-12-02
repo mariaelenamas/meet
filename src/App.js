bcc8ef9a3e7d89e687114d8769a83a7eafd1ffe6
Maria Elena <111201881+MariaElenaMas@users.noreply.github.com>
2023-12-02 18:50:27 +0100

    CityEventsChart added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import CitySearch from "./components/CitySearch";
 import EventList from "./components/EventList";
 import NumberOfEvents from "./components/NumberOfEvents";
+import CityEventsChart from './components/CityEventsChart';
 import { useEffect, useState } from "react";
 import { extractLocations, getEvents } from "./api";
 import { InfoAlert, ErrorAlert, WarningAlert } from "./components/Alert";
@@ -35,19 +36,19 @@ const App = () => {
 
   return (
     <div className="App">
+      <h1>Meet App</h1>
       <div className="alerts-container">
         {infoAlert.length ? <InfoAlert text={infoAlert} /> : null}
         {errorAlert.length ? <ErrorAlert text={errorAlert} /> : null}
         {warningAlert.length ? <WarningAlert text={warningAlert} /> : null}
-        <div>
-          <CitySearch
-            allLocations={allLocations}
-            setCurrentCity={setCurrentCity}
-            setInfoAlert={setInfoAlert} />
-        </div>
-        <NumberOfEvents eventsNumber={events.length} setCurrentNOE={setCurrentNOE} setErrorAlert={setErrorAlert} />
-        <EventList events={events} />
       </div>
+      <CitySearch
+        allLocations={allLocations}
+        setCurrentCity={setCurrentCity}
+        setInfoAlert={setInfoAlert} />
+      <NumberOfEvents eventsNumber={events.length} setCurrentNOE={setCurrentNOE} setErrorAlert={setErrorAlert} />
+      <CityEventsChart allLocations={allLocations} events={events} />
+      <EventList events={events} />
     </div>
   );
 }
